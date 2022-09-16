1bab5c2c9cd2521630998e3369d2f64e6cbd4fc9
deveshpatil619 <75167794+deveshpatil619@users.noreply.github.com>
2022-09-16 18:31:56 +0530

    Updated

---
+++ Binary_search_Assignments/Fixed-point array-Binary Search.py
@@ -0,0 +1,20 @@
+def fixedpoint(arr):
+    low = 0
+    high = len(arr)-1
+
+    while low<=high:
+        mid = low+(high-low)//2
+
+        if arr[mid] ==mid:
+            return arr[mid]
+        elif arr[mid]< mid:
+            low = mid+1
+        else:
+            high = mid-1
+
+    return  None
+
+
+arr = [-10,-5,0,3,7]
+print(fixedpoint(arr))
+
