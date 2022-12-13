cd593be55efc0d9e45372754d3d5ae5cc4faf92d
DEVESH PATIL <deveshpatil619@gmail.com>
2022-12-13 18:30:47 +0530

    Updated

---
+++ Graph_Traversal/BFS_Breadth_First_Search.py
@@ -0,0 +1,52 @@
+## **Breadth First Search Implementation**
+## **Used Data Structure : Queue**
+
+## Graph Representation
+graph = {
+    'A':['B','C','D'],
+    'B':['E'],
+    'C':['E','F'],
+    'D':['F'],
+    'E':['G'],
+    'F':['G'],
+    'G':[]
+}
+
+## creating set to keep track of visited nodes
+
+visited = set()
+
+## queue used to implementation of the BFS
+
+from collections import deque
+queue = deque()
+
+## method definition
+
+def breadth_first_search(visited,graph,node):
+    ## checking if the nodes visited or not and storing them into the visited set
+    if node not in visited:
+        visited.add(node)
+        queue.appendleft(node)
+
+    ## storing the elements into queue and popping
+    while queue:
+        node = queue.pop()
+        print(node,end=" ")
+
+    ## checking for all the adjacent nodes in the level
+
+        for adjacent in graph[node]:
+            if adjacent not in visited:
+                visited.add(adjacent) ## adding to the set
+                queue.appendleft(adjacent) ## adding to the queue
+
+
+
+## Driver_code
+
+breadth_first_search(visited,graph,'A')
+
+
+
+
---
+++ Graph_Traversal/DFS_Deapth_First_Search.py
@@ -0,0 +1,36 @@
+## **Depth First Search Implementation**
+## **Used Data Structure : Stack**
+
+## Graph Representation
+graph = {
+    'A':['B','C','D'],
+    'B':['E'],
+    'C':['E','F'],
+    'D':['F'],
+    'E':['G'],
+    'F':['G'],
+    'G':[]
+}
+
+## creating set to keep track of visited nodes
+
+visited = set()
+
+## method definition
+
+def depth_first_search(visited,graph,node):
+    ## to check if element is present or not
+    if node not in visited:
+        print(node, end=" ")
+        visited.add(node)
+        ## checking for all the adjacent ones
+        for adjacent in graph[node]:
+            depth_first_search(visited,graph,adjacent)
+
+## Driver code
+depth_first_search(visited,graph,'A')
+
+
+## The time complexity of the DFS algorithm is O(V+E), where V is the number of vertices and
+# E is the number of edges in the graph.
+
